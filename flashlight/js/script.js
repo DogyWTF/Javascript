e719b9b94b8361412b1365227783e5323bded208
DogyWTF <63203014+DogyWTF@users.noreply.github.com>
2023-08-02 10:49:17 +0300

    Delete flashlight directory

--- flashlight/js/script.js
+++
@@ -1,6 +0,0 @@
-var light = document.querySelector(".lantern");
-document.addEventListener("mousemove", function(e){
-    var x = e.pageX;
-    var y = e.pageY;
-    light.style.background = "radial-gradient(circle at "+x+"px "+y+"px,transparent,#000 50%)";
-})
